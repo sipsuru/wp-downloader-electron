d07c668efcfaa31ed09a30ac6b236d5fdd0fd8f2
Sipsuru (Dragon Patches) <xxx.dev.dsf@gmail.com>
2024-11-10 23:21:58 +0530

    Initial Upload

---
+++ main.js
@@ -0,0 +1,23 @@
+let express = require("express");
+const app = express();
+
+const Translation = require("./services/translation");
+Translation.loadLanguages();
+
+app.use(express.static("static"));
+
+app.set("view engine", "pug");
+app.set("views", "views");
+
+const api = require("./routes/api");
+const frontend = require("./routes/frontend");
+
+app.use("/api", api);
+app.use(frontend);
+
+// Start the server
+const server = app.listen(3000, () => {
+  console.info("App gestartet auf Port: 3000");
+});
+
+module.exports = server;  // Export the server instance
